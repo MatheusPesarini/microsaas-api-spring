636320e2468376890a2f6756acdce203a5018f5b
Matheus <mrogeriopesarini@gmail.com>
2025-01-30 11:17:35 -0300

    First commit

---
+++ src/main/java/com/pesarini/meu_backend/MeuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pesarini.meu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesarini/meu_backend/MeuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesarini.meu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
